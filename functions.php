62a779831787666918cd86df8ec5e73247d4f1fb
Maksim Palii <maksim.palii@gmail.com>
2018-03-11 17:16:27 +0200

    homework3, replace ' => ", ===

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ function task1()
     $file = file_get_contents('data.xml');
     $xml = new SimpleXMLElement($file);
     ?>
-    <table border="1">
+    <table border='1'>
         <caption>PurchaseOrderNumber: <?php echo $xml->attributes()->PurchaseOrderNumber->__toString(); ?></caption>
         <caption>OrderDate: <?php echo $xml->attributes()->OrderDate->__toString() . PHP_EOL; ?></caption>
         <tr>
@@ -17,16 +17,16 @@ function task1()
         </tr>
 
         <?php
-        echo "<tr>";
+        echo '<tr>';
         foreach ($xml->Items as $key => $value) {
             foreach ($value as $keys => $values) {
-                echo "<td>" . $values->attributes()->PartNumber . "</td>";
-                echo "<td>" . $values->ProductName . "</td>";
-                echo "<td>" . $values->Quantity . "</td>";
-                echo "<td>" . $values->USPrice . "</td>";
-                echo "<td>" . $values->ShipDate . "</td>";
-                echo "<td>" . $values->Comment . "</td>";
-                echo "</tr>";
+                echo '<td>' . $values->attributes()->PartNumber . '</td>';
+                echo '<td>' . $values->ProductName . '</td>';
+                echo '<td>' . $values->Quantity . '</td>';
+                echo '<td>' . $values->USPrice . '</td>';
+                echo '<td>' . $values->ShipDate . '</td>';
+                echo '<td>' . $values->Comment . '</td>';
+                echo '</tr>';
             }
         }
         ?>
@@ -34,19 +34,19 @@ function task1()
     <h3>Adress</h3>
     <?php
     foreach ($xml->Address as $key => $value) {
-        echo "<p><b>" . $value->attributes()->Type->__toString() . "</b><br>";
-        echo $value->Name->__toString() . "<br>" . $value->Street->__toString() . "<br>";
-        echo $value->City->__toString() . ", " . $value->State->__toString() . ", " . $value->Country->__toString() . " " . $value->Zip->__toString() . "</p>";
+        echo '<p><b>' . $value->attributes()->Type->__toString() . '</b><br>';
+        echo $value->Name->__toString() . '<br>' . $value->Street->__toString() . '<br>';
+        echo $value->City->__toString() . ', ' . $value->State->__toString() . ', ' . $value->Country->__toString() . ' ' . $value->Zip->__toString() . '</p>';
     }
 
-    echo "<h4>DeliveryNotes</h4>";
+    echo '<h4>DeliveryNotes</h4>';
     echo $xml->DeliveryNotes->__toString();
 }
 
 function task2()
 {
-    $product = ["ProductName" => "Lawnmower", "Quantity" => 1, "USPrice" => 148.95];
-    $data = ["Name" => "Ellen Adams", "Street" => "123 Maple Street", "Product" => $product];
+    $product = ['ProductName' => 'Lawnmower', 'Quantity' => 1, 'USPrice' => 148.95];
+    $data = ['Name' => 'Ellen Adams', 'Street' => '123 Maple Street', 'Product' => $product];
     $encoded = json_encode($data, JSON_UNESCAPED_UNICODE);
     file_put_contents('output.json', $encoded);
 
@@ -55,14 +55,14 @@ function task2()
 
     switch (random_int(1, 3)) {
         case 1:
-            $decoded["Product"]["USPrice"] += random_int(3, 20);
-            $decoded["Product"]["Quantity"] += random_int(1, 4);
+            $decoded['Product']['USPrice'] += random_int(3, 20);
+            $decoded['Product']['Quantity'] += random_int(1, 4);
             break;
         case 2:
-            $decoded["Name"] = "Tai Yee";
+            $decoded['Name'] = 'Tai Yee';
             break;
         default:
-            echo "Данные не изменились!";
+            echo 'Данные не изменились!';
     }
 
     $encoded = json_encode($decoded, JSON_UNESCAPED_UNICODE);
@@ -76,15 +76,15 @@ function task2()
 
 
     foreach ($arrDecoded as $value => $key) {
-        if ($arr2Decoded[$value] != $arrDecoded[$value]) {
+        if ($arr2Decoded[$value] !== $arrDecoded[$value]) {
             if (is_array($arr2Decoded[$value])) {
                 foreach ($arrDecoded[$value] as $val => $key2) {
-                    if ($arr2Decoded[$value][$val] != $arrDecoded[$value][$val]) {
-                        echo $val . ": " . $arr2Decoded[$value][$val] . PHP_EOL;
+                    if ($arr2Decoded[$value][$val] !== $arrDecoded[$value][$val]) {
+                        echo $val . ': ' . $arr2Decoded[$value][$val] . PHP_EOL;
                     }
                 }
             } else {
-                echo $value . ": " . $arr2Decoded[$value];
+                echo $value . ': ' . $arr2Decoded[$value];
             }
         }
     }
@@ -100,7 +100,7 @@ function task3()
     }
     $fp = fopen($csvPath, 'w');
 
-    fputcsv($fp, $data, ",");
+    fputcsv($fp, $data, ',');
     fclose($fp);
 
     $csvFile = fopen($csvPath, 'r');
@@ -109,7 +109,6 @@ function task3()
         while (($csvData = fgetcsv($csvFile, filesize($csvPath), ',')) !== false) {
             $res[] = $csvData;
         }
-        //print_r($res);
         $summa = 0;
         foreach ($res as $val) {
             foreach ($val as $numb) {
@@ -118,7 +117,7 @@ function task3()
                 }
             }
         }
-        echo "Сумма четных чисел: " . $summa;
+        echo 'Сумма четных чисел: ' . $summa;
     }
 }
 
@@ -128,9 +127,9 @@ function task4()
     $data = file_get_contents('https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json');
     $dataDecoded = json_decode($data, true);
 
-    echo "Вывод через ключ<br>" . PHP_EOL;
-    echo "Pageid: " . $dataDecoded['query']['pages']['15580374']['pageid'] . "<br>" . PHP_EOL;
-    echo "Title: " . $dataDecoded['query']['pages']['15580374']['title'] . PHP_EOL;
+    echo 'Вывод через ключ<br>' . PHP_EOL;
+    echo 'Pageid: ' . $dataDecoded['query']['pages']['15580374']['pageid'] . '<br>' . PHP_EOL;
+    echo 'Title: ' . $dataDecoded['query']['pages']['15580374']['title'] . PHP_EOL;
 
     function foundInArray()
     {
@@ -138,8 +137,8 @@ function task4()
         if (is_array($dats) || is_object($dats)) {
             foreach ($dats as $val) {
                 if (is_array($val) && array_key_exists('pageid', $val)) {
-                    echo "Pageid: " . $val['pageid'] . "<br>" . PHP_EOL;
-                    echo "Title: " . $val['title'];
+                    echo 'Pageid: ' . $val['pageid'] . '<br>' . PHP_EOL;
+                    echo 'Title: ' . $val['title'];
                 } else {
                     foundInArray($val);
                 }
@@ -147,7 +146,7 @@ function task4()
         }
     }
 
-    echo "<br><br>" . PHP_EOL;
-    echo "Через поиск<br>";
+    echo '<br><br>' . PHP_EOL;
+    echo 'Через поиск<br>';
     foundInArray($dataDecoded);
 }
--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-require "functions.php";
+require 'functions.php';
 task1();
-echo "<br><br>";
+echo '<br><br>';
 task2();
-echo "<br><br>";
+echo '<br><br>';
 task3();
-echo "<br><br>";
+echo '<br><br>';
 task4();
